2fec8b707b54d7cfe2fbd87e37ead2aba6badcc9
Ryan Vu <ryanvu@frc4322.com>
2023-01-14 11:54:44 -0800

    Started on Arm.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,4 +2,5 @@ package frc.robot;
 
 public class Constants {
   
+    public static final boolean armEnabled = true;
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+
+import frc.robot.Constants;
+
 public class Arm {
-  
+  private CANSparkMax armMotor1;
+  private SparkMaxPIDController armPID;
+
+  private Arm() { 
+    if (Constants.armEnabled) {
+        
+    }
+  }
 }
