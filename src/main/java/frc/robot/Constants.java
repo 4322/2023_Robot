0ac24fce78c0c534dbab76b062b8072d290e1008
Ryan Vu <ryanvu@frc4322.com>
2023-01-14 13:37:07 -0800

    Started writing some methods. breakMode, coastMode, setCurrentPosition, rotate, rotateToPos (not done)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,4 +3,9 @@ package frc.robot;
 public class Constants {
   
     public static final boolean armEnabled = true;
+
+    public static final class ArmConstants {
+        public static final int motorID = 0;
+        public static final double rampRate = 0.0;
+    }
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -2,16 +2,61 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import frc.robot.Constants;
 
 public class Arm {
-  private CANSparkMax armMotor1;
+  private CANSparkMax leftMotor;
   private SparkMaxPIDController armPID;
+
+  private enum rotationDir {
+    forward,
+    backward,
+    none
+}
 
   private Arm() { 
     if (Constants.armEnabled) {
-        armMotor1 = new CANSparkMax(0, null);
+        leftMotor = new CANSparkMax(Constants.ArmConstants.motorID, MotorType.kBrushless);
+    }
+    
+    public void init() {
+        if (Constants.armEnabled) {
+            leftMotor.restoreFactoryDefaults();
+            leftMotor.setIdleMode(IdleMode.kCoast);
+            leftMotor.setOpenLoopRampRate(Constants.ArmConstants.rampRate);
+            armEncoder = armEncoder.getEncoder();
+        }
+    }
+
+    
+
+    public void rotateToPos(double targetPos) {
+
+    }
+
+    public void rotate() {
+        if (Constants.armEnabled) {
+            
+        }
+    }
+
+    public void setCurrentPosition(double pos) {
+        leftMotor.set
+    }
+
+    public void setCoastMode() {
+        if (Constants.armEnabled) {
+            leftMotor.setIdleMode(IdleMode.kCoast);
+        }
+    }
+
+    public void setBreakMode() {
+        if (Constants.armEnabled) {
+            leftMotor.setIdleMode(IdleMode.kBrake);
+        }
     }
   }
 }
