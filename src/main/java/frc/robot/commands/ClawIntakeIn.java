4079308149d4adfb6d4a9d00c82ebe70d103b593
Amelia Schroeder <ameliaschroeder@frc4322.com>
2023-01-15 11:27:05 -0800

    created init func for claw

--- src/main/java/frc/robot/commands/ClawIntakeIn.java
+++ src/main/java/frc/robot/commands/ClawIntakeIn.java
@@ -1,7 +1,13 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import frc.robot.subsystems.Claw;
 public class ClawIntakeIn extends CommandBase {
-
+  private Claw claw;
+  public ClawIntakeIn(Claw clawSubsystem)
+  {
+    claw = clawSubsystem;
+    //addRequirements(claw);
+  }
 }
