39046a60752cf7bbeccd4fff473ce63fed6d626a
Dhrish Karangula <dhrishkarangula@frc4322.com>
2023-01-22 13:30:55 -0800

    Reverted the constants to include DriveConstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,26 +1,55 @@
 package frc.robot;
 
 public class Constants {
-  
-  public static final boolean armEnabled = true;
-  public static final boolean clawEnabled = true;
+  public static final boolean armEnabled = false;
+  public static final boolean clawEnabled = false;
+  public static final boolean driveEnabled = false;
+  public static final boolean gyroEnabled = false;
 
-  public static final class ArmConstants {
-    public static final int motorID = 0;
-    public static final double rampRate = 0.0;
-    public static final double forward = 1;
-    public static final double backward = -1;
-    public static final int positionTolerance = 100;
+  public static final class DriveConstants{
+    public static final int frontRightDriveID = 0;
+    public static final int frontRightRotationID = 0;
+    public static final int rearRightDriveID = 0;
+    public static final int rearRightRotationID = 0;
+    public static final int frontLeftDriveID = 0;
+    public static final int frontLeftRotationID = 0;
+    public static final int rearLeftDriveID = 0;
+    public static final int rearLeftRotationID = 0;
+
+    public static final int frontRightEncoder = 0;
+    public static final int rearRightEncoder = 0;
+    public static final int frontLeftEncoder = 0;
+    public static final int rearLeftEncoder = 0;
+
+    public static final int encoderResolution = 0;
+
+    public static final double distWheelMetersX = 0;
+    public static final double distWheelMetersY = 0;
+
+    public static final double wheelBaseLengthFeet = 0;
+    public static final double wheelBaseWidthFeet = 0;
+
+    public static final double maxSpeedMetersSecond = 0;
+    public static final double maxRotationSpeedRadSecond = 0;
+
+    public static final class Rotation {
+      
+    }
+
+    public static final class Drive {
+
+    }
   }
-    
+
   public static final class ClawConstants {
     public static final int motorID = 1; //temp value
     public static final double rampRate = 0.8; //temp value
   }
   
-    public static final class ArmConstants {
-        public static final int motorID = 0;
-        public static final double rampRate = 0.0;
-        public static final double forward = 1;
-        public static final double backward = -1;
+  public static final class ArmConstants {
+      public static final int motorID = 0;
+      public static final double rampRate = 0.0;
+      public static final double forward = 1;
+      public static final double backward = -1;
+  }
 }
