3cb453a856f4b5de6588c2d259a35b78f5c25827
Ryan Vu <ryanvu@frc4322.com>
2023-01-28 14:45:58 -0800

    Finished ClawIntakeOut

--- src/main/java/frc/robot/commands/ClawIntakeOut.java
+++ src/main/java/frc/robot/commands/ClawIntakeOut.java
@@ -2,6 +2,35 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Claw;
+
 public class ClawIntakeOut extends CommandBase{
-  
+  private Claw claw; 
+
+  public ClawIntakeOut(Claw clawSubsystem) {
+    claw = clawSubsystem;
+
+    addRequirements(claw);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    claw.manualOuttake();
+  }
+
+  @Override
+  public void execute() {
+  }
+
+  //Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    claw.manualStop();
+  }
+
+  // Run until interrupted
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
