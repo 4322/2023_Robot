3bd042c90947ad576b760ea4d082ddd7231d7593
Ryan Vu <ryanvu@frc4322.com>
2023-01-28 14:20:15 -0800

    Started on clawIntakeIn

--- src/main/java/frc/robot/commands/ClawIntakeIn.java
+++ src/main/java/frc/robot/commands/ClawIntakeIn.java
@@ -3,11 +3,36 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Claw;
+
 public class ClawIntakeIn extends CommandBase {
   private Claw claw;
   public ClawIntakeIn(Claw clawSubsystem)
   {
     claw = clawSubsystem;
-    //addRequirements(claw);
+
+    // addRequirements(claw);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    
+  }
+
+  // Run until interrupted
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }
