b032172aad6d202bdc3990dd8f179c3dcac01ee7
amelia-schroeder <115679096+amelia-schroeder@users.noreply.github.com>
2023-01-29 10:38:59 -0800

    Update Constants.java
    moved arm constant back into arm constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -150,9 +150,7 @@ public class Constants {
       public static final double kFF = 0.05;
     }
 
-    public static final double logIntervalSeconds = 1;
-    public static final int maxPosition = 1000;
-    public static final int minPosition = -1000;
+ 
   }
 
   public static final class ArmConstants {
@@ -160,6 +158,9 @@ public class Constants {
     public static final double rampRate = 0.0;
     public static final double forward = 1;
     public static final double backward = -1;
+    public static final double logIntervalSeconds = 1;
+    public static final int maxPosition = 1000;
+    public static final int minPosition = -1000;
   }
 
   public static final class ClawConstants {
