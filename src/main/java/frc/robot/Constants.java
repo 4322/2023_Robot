199687a55413f379c30d3e9cfa7477e808c10b66
Ryan Vu <ryanvu@frc4322.com>
2023-02-03 19:18:19 -0800

    Fixed Merge errors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -172,20 +172,12 @@ public class Constants {
     public static final double backward = -1;
     public static final double logIntervalSeconds = 1;
     public static final int positionTolerance = 100;
-<<<<<<< HEAD
 
     public static enum ArmDirection {
       forwards, stationary, backwards
     };
-  }
-    
-  public static final class ClawConstants {
-    public static final int motorID = 1; //temp value
-    public static final double rampRate = 0.8; //temp value
-  }
-=======
+  
     public static final int maxPosition = 1000;
     public static final int minPosition = -1000;
   }
->>>>>>> dev
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,10 +7,11 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-<<<<<<< HEAD
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.ArmConstants.ArmDirection;
 
 public class Arm extends SubsystemBase {
@@ -21,45 +22,14 @@ public class Arm extends SubsystemBase {
   public Arm() {
     if (Constants.armEnabled) {
       motor = new CANSparkMax(Constants.ArmConstants.motorID, MotorType.kBrushless);
-=======
-import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.Constants.ArmConstants;
-
-public class Arm extends SubsystemBase {
-  private CANSparkMax leftMotor;
-  private CANSparkMax rightMotor;
-  private Double currentTarget = null;
-  private Timer logTimer = new Timer();
-
-  public Arm() {
-    if (Constants.armEnabled) {
-      leftMotor = new CANSparkMax(ArmConstants.motorID, MotorType.kBrushless);
-      rightMotor = new CANSparkMax(ArmConstants.motorID, MotorType.kBrushless);
->>>>>>> dev
     }
   }
 
   public void init() {
     if (Constants.armEnabled) {
-<<<<<<< HEAD
       motor.restoreFactoryDefaults();
       motor.setIdleMode(IdleMode.kCoast);
       motor.setOpenLoopRampRate(Constants.ArmConstants.rampRate);
-=======
-      leftMotor.restoreFactoryDefaults();
-      leftMotor.setIdleMode(IdleMode.kCoast);
-      leftMotor.setOpenLoopRampRate(ArmConstants.rampRate);
-      rightMotor.restoreFactoryDefaults();
-      rightMotor.setIdleMode(IdleMode.kCoast);
-      rightMotor.setOpenLoopRampRate(ArmConstants.rampRate);
-      rightMotor.follow(leftMotor);
-      rightMotor.setInverted(true);
-      logTimer.reset();
-      logTimer.start();
->>>>>>> dev
     }
   }
 
@@ -80,46 +50,26 @@ public class Arm extends SubsystemBase {
 
   public boolean rotateToPosition(double targetPosition) {
     if (Constants.armEnabled) {
-<<<<<<< HEAD
-      motor.getPIDController().setReference(targetPosition, ControlType.kPosition);
-      currentTarget = targetPosition;
-=======
       if ((targetPosition > ArmConstants.minPosition)
           && (targetPosition < ArmConstants.maxPosition)) {
-        leftMotor.getPIDController().setReference(targetPosition, ControlType.kPosition);
+        motor.getPIDController().setReference(targetPosition, ControlType.kPosition);
         currentTarget = targetPosition;
         DataLogManager
             .log("Rotating to position " + currentTarget + " from position " + getPosition());
         return true;
       }
->>>>>>> dev
     }
     return false;
   }
 
   public void rotateForward() {
-<<<<<<< HEAD
-    motor.set(Constants.ArmConstants.forward);
-    armDirection = ArmDirection.forwards;
-  }
-
-  public void rotateBackward() {
-    motor.set(Constants.ArmConstants.backward);
-    armDirection = ArmDirection.backwards;
-  }
-
-  public void stop() {
-    motor.set(0);
-    armDirection = ArmDirection.stationary;
-=======
-    leftMotor.set(ArmConstants.forward);
+    motor.set(ArmConstants.forward);
     DataLogManager.log("Arm rotating forward");
   }
 
   public void rotateBackward() {
-    leftMotor.set(ArmConstants.backward);
+    motor.set(ArmConstants.backward);
     DataLogManager.log("Arm rotating backward");
->>>>>>> dev
   }
 
   public void setCoastMode() {
