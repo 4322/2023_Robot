e9ac5c7cd7cb21824483ad2a518b5e88509514e4
Ryan Vu <ryanvu@frc4322.com>
2023-02-03 19:09:48 -0800

    Configure button bindings in robot container

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class Constants {
   public static final boolean debug = false;
   public static final boolean inDemoMode = false;
+  public static final boolean joysticksEnabled = false;
 
   public static final boolean armEnabled = false;
   public static final boolean clawEnabled = false;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Claw;
+import frc.robot.subsystems.Drive;
 import frc.robot.commands.*;
 
 public class RobotContainer {
@@ -12,25 +15,52 @@ public class RobotContainer {
 
   // Define controllers
   private final XboxController xbox = new XboxController(2);
+  public static Joystick driveStick;
+  public static Joystick rotateStick;
+
+
 
   // The robot's subsystems and commands are defined here...
   private final Arm arm = new Arm();
   private final Claw claw = new Claw();
+  private final Drive drive = new Drive();
 
   // Arm commands
+
   // Claw commands
   private final ClawIntake clawIntake = new ClawIntake(claw);
   private final ClawOuttake clawOuttake = new ClawOuttake(claw);
+
+  // Drive Commands
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
 
   public RobotContainer() {
 
+    // Initialize subsystems that use CAN after all of them have been constructed because the
+    // constructors lower CAN bus utilization to make configuration reliable.
+    drive.init();
+    arm.init();
+    claw.init();
+
+    // Conifigure the button bindings
+    configureButtonBindings();
   }
 
-  // Initialize subsystems that use CAN after all of them have been constructed because the
-  // constructors lower CAN bus utilization to make configuration reliable.
 
-  // Configure the button bindings
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+    private void configureButtonBindings() {
+      
+      if (Constants.joysticksEnabled) {
+        driveStick = new Joystick(0);
+        rotateStick = new Joystick(1);
+      }
+    }
   
   // stagger status frame periods to reduce peak CAN bus utilization
   private static int nextFastStatusPeriodMs = Constants.fastStatusPeriodBaseMs;
