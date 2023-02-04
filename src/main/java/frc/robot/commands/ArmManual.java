ab41d0f6dd3c38d4749cc27bfc576ab279e23c9b
Ryan Vu <ryanvu@frc4322.com>
2023-02-03 18:38:04 -0800

    did a little in armManual. Nothing important

--- src/main/java/frc/robot/commands/ArmManual.java
+++ src/main/java/frc/robot/commands/ArmManual.java
@@ -1,11 +1,13 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
 public class ArmManual extends CommandBase {
 
   private final Arm arm;
+  private double speed;
 
   public ArmManual(Arm armsubsystem) {
     arm = armsubsystem;
@@ -22,13 +24,13 @@ public class ArmManual extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+  
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+    arm.stop();
   }
 
   // Returns true when the command should end.
