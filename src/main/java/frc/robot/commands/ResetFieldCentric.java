d813d640eb9c4de4952b71c439859a239f183295
Amelia Schroeder <ameliaschroeder@frc4322.com>
2023-02-04 11:05:29 -0800

    added resetfieldcentric function

---
+++ src/main/java/frc/robot/commands/ResetFieldCentric.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class ResetFieldCentric extends InstantCommand {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+  private final Drive driveSubsystem;
+  private final double offset;
+  private final boolean runWhenEnabled;
+
+  public ResetFieldCentric(Drive driveSubsystem, double offset, boolean runWhenEnabled) {
+    this.driveSubsystem = driveSubsystem;
+    this.offset = offset;
+    this.runWhenEnabled = runWhenEnabled;
+    // no need to interrupt other commands when changing drive mode
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (DriverStation.isDisabled() || runWhenEnabled) {
+      driveSubsystem.resetFieldCentric(offset);
+      String report = "Reset Field Centric for " + offset + " degrees";
+      DriverStation.reportWarning(report, false);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;  // allow field orientation to be set before start of match
+  }
+}
