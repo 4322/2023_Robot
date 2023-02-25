6cd7aa82825e5b50dcd1b97d0e2d596bd833200b
Ryan Vu <ryanvu@frc4322.com>
2023-02-25 13:35:36 -0800

    changed methods and added shuffleboard

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -4,6 +4,7 @@ import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -17,21 +18,29 @@ public class ColorSensor extends SubsystemBase{
   private final Color kYellowTarget = new Color(0.992, 0.792, 0.329);
   private final Color kPurpleTarget = new Color(0.298, 0.086, 0.761);
   private final Color kOrangeTarget = new Color(1, 0.576, 0.020);
-  
+
   public ColorSensor() {
-    Color detectedColor = colorSensor.getColor();
+    if (Constants.colorSensorEnabled) {
+      Color detectedColor = colorSensor.getColor();
 
-    String colorString;
-    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+      String colorString;
+      ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
 
-    if (match.color == kOrangeTarget) {
-      colorString = "Orange";
-    } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
-    } else if (match.color == kPurpleTarget) {
-      colorString = "Purple";
-    } else {
-      colorString = "Unknown";
+      if (match.color == kOrangeTarget) {
+        colorString = "Orange";
+      } else if (match.color == kYellowTarget) {
+        colorString = "Yellow";
+      } else if (match.color == kPurpleTarget) {
+        colorString = "Purple";
+      } else {
+        colorString = "Unknown";
+      }
+
+    SmartDashboard.putNumber("Red", detectedColor.red); 
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
     }
   }
 
@@ -43,16 +52,8 @@ public class ColorSensor extends SubsystemBase{
     }
   }
 
-  public Color getYellow() {
-    return kYellowTarget;
-  }
-
-  public Color getPurple() {
-    return kPurpleTarget;
-  }
-
-  public Color getOrange() {
-    return kOrangeTarget;
+  public Color getColor() {
+    return colorSensor.getColor();
   }
 }
   
