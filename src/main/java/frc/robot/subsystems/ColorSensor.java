3474693f5098776d360cc2c9d9a4fd3c854c3411
Ryan Vu <ryanvu@frc4322.com>
2023-02-25 12:52:13 -0800

    Created colorSensor file

---
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ColorSensor extends SubsystemBase{
+
+  public ColorSensor() {
+    
+  }
+
+  public void init() {
+    
+  }
+}
+  
