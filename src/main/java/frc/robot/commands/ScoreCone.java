43c7d8648ebd21d98e69378ad75f9e4989c182eb
Ryan Vu <ryanvu@frc4322.com>
2023-03-04 17:40:39 -0800

    scoreCone

---
+++ src/main/java/frc/robot/commands/ScoreCone.java
@@ -0,0 +1,75 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
+
+public class ScoreCone extends CommandBase{
+  private Arm arm;
+  private Claw claw;
+  private scoreMode currentMode;
+  private Timer timer = new Timer();
+
+  public enum scoreMode {
+    rotating,
+    atPosition,
+    done,
+    abort;
+  }
+
+  public ScoreCone(Arm armSubsystem, Claw clawSubsystem) {
+    arm = armSubsystem;
+    claw = clawSubsystem;
+
+    addRequirements(arm, claw);
+  }
+
+  @Override
+  public void initialize() {
+    currentMode = scoreMode.rotating;
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void execute() {
+    switch (currentMode) {
+      case rotating:
+        if (Constants.armEnabled) {
+          arm.rotateToPosition(Constants.ArmConstants.MidScoringPosition);
+          if (arm.isAtTarget()) {
+            currentMode = scoreMode.atPosition;
+          }
+        } else {
+          currentMode = scoreMode.done;
+        }
+      case atPosition:
+        if (Constants.clawEnabled) {
+          claw.outtake();
+        }
+
+        if (timer.hasElapsed(1)) {
+          currentMode = scoreMode.done;
+        }
+      case done: // falls to break
+      case abort:
+        break;
+    }
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    arm.stop();
+    claw.stop();
+    currentMode = scoreMode.done;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return ((currentMode == scoreMode.done) || (currentMode == scoreMode.abort));
+  }
+}
