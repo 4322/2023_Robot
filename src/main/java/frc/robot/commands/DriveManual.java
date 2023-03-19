45b8f1703330ed3f73763eeb667c201ca9126ae1
Ryan Vu <ryanvu@frc4322.com>
2023-03-18 19:15:02 -0700

    integrated spinout into drivemanual

--- src/main/java/frc/robot/commands/DriveManual.java
+++ src/main/java/frc/robot/commands/DriveManual.java
@@ -23,13 +23,7 @@ public class DriveManual extends CommandBase {
   private spinoutMode currentMode;
 
   public enum spinoutMode {
-    none,
-    frontLeft,
-    backLeft,
-    backRight,
-    frontRight,
-    done,
-    abort;
+    none, frontLeft, backLeft, backRight, frontRight, abort;
   }
 
   public DriveManual(Drive drivesubsystem, Double targetHeadingDeg) {
@@ -45,7 +39,7 @@ public class DriveManual extends CommandBase {
   public void initialize() {
     drive.resetRotatePID();
     currentMode = spinoutMode.none;
-    done = false;  // make command reusable
+    done = false; // make command reusable
   }
 
   @Override
@@ -92,6 +86,7 @@ public class DriveManual extends CommandBase {
       final double drive2Deadband = Manual.xboxDriveDeadband;
       final double rotate2LeftDeadband = Manual.xboxRotateDeadband;
       final double rotate2RightDeadband = Manual.xboxRotateDeadband;
+      final double rotateSecondDeadBand = Manual.spinoutRotateDeadBand;
 
       // Convert raw drive inputs to polar coordinates for more precise deadband correction
       final double drive1RawMag = OrangeMath.pythag(drive1RawX, drive1RawY);
@@ -99,19 +94,21 @@ public class DriveManual extends CommandBase {
       final double drive1RawTheta = Math.atan2(drive1RawY, drive1RawX);
       final double drive2RawTheta = Math.atan2(drive2RawY, drive2RawX);
 
+      final double drivebaseAngle = drive.getAngle();
+
       // Normalize the drive inputs over deadband in polar coordinates.
-      // Process each set of inputs separately to avoid a discontinuity  
+      // Process each set of inputs separately to avoid a discontinuity
       // when the second input suddenly exceeds deadband.
       double drive1Mag = 0;
       if (drive1RawMag > drive1Deadband) {
         drive1Mag = (drive1RawMag - drive1Deadband) / (1 - drive1Deadband);
-        drive1Mag = drive1Mag * drive1Mag * drive1Mag;  // Increase sensitivity efficiently
-      } 
+        drive1Mag = drive1Mag * drive1Mag * drive1Mag; // Increase sensitivity efficiently
+      }
       double drive2Mag = 0;
       if (drive2RawMag > drive2Deadband) {
         drive2Mag = (drive2RawMag - drive2Deadband) / (1 - drive2Deadband);
-        drive2Mag = drive2Mag * drive2Mag * drive2Mag;  // Increase sensitivity efficiently
-      } 
+        drive2Mag = drive2Mag * drive2Mag * drive2Mag; // Increase sensitivity efficiently
+      }
       // Convert back to cartesian coordinates for proper vector addition
       double drive1X = Math.cos(drive1RawTheta) * drive1Mag;
       double drive1Y = Math.sin(drive1RawTheta) * drive1Mag;
@@ -139,7 +136,7 @@ public class DriveManual extends CommandBase {
       }
 
       // Normalize the rotation inputs over deadband.
-      // Process each input separately to avoid a discontinuity  
+      // Process each input separately to avoid a discontinuity
       // when the second input suddenly exceeds deadband.
       double rotatePower1 = 0;
       if (rotate1Raw > rotate1LeftDeadband) {
@@ -147,7 +144,7 @@ public class DriveManual extends CommandBase {
       } else if (rotate1Raw < -rotate1RightDeadband) {
         rotatePower1 = (rotate1Raw + rotate1RightDeadband) / (1 - rotate1RightDeadband);
       }
-      rotatePower1 = rotatePower1 * rotatePower1 * rotatePower1;  // Increase sensitivity efficiently
+      rotatePower1 = rotatePower1 * rotatePower1 * rotatePower1; // Increase sensitivity efficiently
 
       double rotatePower2 = 0;
       if (rotate2Raw > rotate2LeftDeadband) {
@@ -155,8 +152,9 @@ public class DriveManual extends CommandBase {
       } else if (rotate2Raw < -rotate2RightDeadband) {
         rotatePower2 = (rotate2Raw + rotate2RightDeadband) / (1 - rotate2RightDeadband);
       }
-      rotatePower2 = rotatePower2 * rotatePower2 * rotatePower2;  // Increase sensitivity efficiently
+      rotatePower2 = rotatePower2 * rotatePower2 * rotatePower2; // Increase sensitivity efficiently
 
+      double driveTheta = Math.atan2(driveY, driveX);
       // Cap the combined rotation power
       double rotatePower = rotatePower1 + rotatePower2;
       if (rotatePower > 1) {
@@ -165,106 +163,102 @@ public class DriveManual extends CommandBase {
         rotatePower = -1;
       }
 
+      if (Math.abs(rotatePower) > Manual.spinoutRotateDeadBand) {
+        secondDeadBandTimer.start();
+      } else {
+        secondDeadBandTimer.restart();
+      }
+
       if (targetHeadingDeg == null) {
-        drive.drive(driveX, driveY, rotatePower);
+        // Go on to spinout code
       } else if (rotatePower == 0) {
         drive.driveAutoRotate(driveX, driveY, targetHeadingDeg,
             Constants.DriveConstants.Manual.rotateToleranceDegrees);
+        return;
       } else {
-        // break out of auto-rotate and return to default command
-       drive.drive(driveX, driveY, rotatePower);
-       done = true;
-      }
-      
-      if (rotate1Raw > Constants.DriveConstants.Manual.spinoutRotateToleranceDegrees) {
-        secondDeadBandTimer.start();
-      } else {
-        secondDeadBandTimer.restart();
+        // break out of auto-rotate and return to spinout check
+        done = true;
       }
 
-      if (rotate2Raw > Constants.DriveConstants.Manual.spinoutRotateToleranceDegrees) {
-        secondDeadBandTimer.start();
-      } else {
-        secondDeadBandTimer.restart();
-      }
+      // detect if not rotating and if rotate stick past second deadband for certain amount of
+        // time
+        // (first deadband is rotateToleranceDegrees/xboxRotateDeadband)
+        // (second deadband is past first deadband in rotation) (close to max rotation)
+        if (
+        // drive.getAngleVelocity() < Constants.DriveConstants.Manual.spinoutMinAngleVelocity &&
+        secondDeadBandTimer
+            .hasElapsed(Constants.DriveConstants.Manual.spinoutSecondDeadBandThreshold)) {
+          // from this, figure out which swerve module to lock onto to rotate off of (use drive
+          // stick direction and robotAngle)
+          // How to use drive stick: module closest to direction of drivestick.
+          // use gyro to find orientation
+          // algorithm to determine quadrant: driveStickAngle - robotAngle (TBD)
+          // if drivestick angle 0 < x < 90 , in quadrant 1 (front left module)
+          // if drivestick angle 90 < x < 180 , in quadrant 2 (back left module)
+          // if drivestick angle -180 < x < -90 , in quadrant 3 (back right module)
+          // if drivestick angle -90 < x < 0 , in quadrant 4 (front right module)
 
-      
-      // detect if not rotating and if rotate stick past second deadband for certain amount of time
-      //    (first deadband is rotateToleranceDegrees/xboxRotateDeadband)
-      //    (second deadband is past first deadband in rotation) (close to max rotation)
-      if (drive.getAngleVelocity() < Constants.DriveConstants.Manual.spinoutMinAngleVelocity &&
-          secondDeadBandTimer.hasElapsed(Constants.DriveConstants.Manual.spinoutSecondDeadBandThreshold)) {
-            // from this, figure out which swerve module to lock onto to rotate off of (use drive stick direction and robotAngle)
-            //    How to use drive stick: module closest to direction of drivestick. 
-            //      use gyro to find orientation
-            //      algorithm to determine quadrant: driveStickAngle - robotAngle (TBD)
-            //        if drivestick angle 0 < x < 90 , in quadrant 1 (front left module)
-            //        if drivestick angle 90 < x < 180 , in quadrant 2 (back left module)
-            //        if drivestick angle -180 < x < -90 , in quadrant 3 (back right module)
-            //        if drivestick angle -90 < x < 0 , in quadrant 4 (front right module)
+          // SPECIAL CASE: if driveStickAngle - robotAngle is exactly 0, 90, 180, -180, then use the
+          // rotate angle to determine wheel:
+          // 0: if CW, quadrant 1 (front left); if CCW, quadrant 4 (front right)
+          // 90: if CW, quadrant 2 (back left); if CCW, quadrant 1 (front left)
+          // 180/-180: if CW, quadrant 3 (back right); if CCW, quadrant 2 (back left)
+          // -90: if CW, quadrant 4 (front right); if CCW, quadrant 3 (back right)
 
-            // Quad 1
-            if ((drive1Mag - drive.getAngle() > 0 && drive1Mag - drive.getAngle() < 90) || 
-                 drive2Mag - drive.getAngle() > 0 && drive2Mag - drive.getAngle() < 90) {
-                  currentMode = spinoutMode.frontLeft;
-            }
+          double angleChange = driveTheta - drivebaseAngle;
 
-            // Quad 2
-            if ((drive1Mag - drive.getAngle() > 90 && drive1Mag - drive.getAngle() < 180) || 
-                 drive2Mag - drive.getAngle() > 90 && drive2Mag - drive.getAngle() < 180) {
-                  currentMode = spinoutMode.backLeft;
-            }
-
-            // Quad 3
-            if ((drive1Mag - drive.getAngle() > -180 && drive1Mag - drive.getAngle() < -90) || 
-                 drive2Mag - drive.getAngle() > -180 && drive2Mag - drive.getAngle() < -90) {
-                  currentMode = spinoutMode.backRight;
-            }
-
-            //Quad 4
-            if ((drive1Mag - drive.getAngle() > -90 && drive1Mag - drive.getAngle() < 0) || 
-                 drive2Mag - drive.getAngle() > -90 && drive2Mag - drive.getAngle() < 0) {
-                  currentMode = spinoutMode.frontRight;
-            }
-
-            //done
-            if ((rotate1Raw < Constants.DriveConstants.Manual.spinoutRotateToleranceDegrees) ||
-              rotate2Raw < Constants.DriveConstants.Manual.spinoutRotateToleranceDegrees) {
-                currentMode = spinoutMode.done;
-              }
-              
-
-            // use state machine for rotating each wheel in each direction (8 cases)
-            //    each module rotating CW and CCW
-            //      if rotation stick falls under second deadband or robot rotates 90 degrees, 
-            //      reset rotation back to normal
-            switch (currentMode) {
-              case none:
-                break;
-              case frontLeft:
-                drive.drive(driveX, driveY, rotatePower, Constants.DriveConstants.frontLeftWheelLocation);
-                break;
-              case backLeft:
-                drive.drive(driveX, driveY, rotatePower, Constants.DriveConstants.backLeftWheelLocation);
-                break;
-              case backRight:
-                drive.drive(driveX, driveY, rotatePower, Constants.DriveConstants.backRightWheelLocation);
-                break;
-              case frontRight:
-                drive.drive(driveX, driveY, rotatePower, Constants.DriveConstants.frontRightWheelLocation);
-                break;
-              case done:
-                drive.drive(driveX, driveY, rotatePower);
-                break;
-              case abort:
-                break;
-            } // pain :(
-            // SPECIAL CASE: if driveStickAngle - robotAngle is exactly 0, 90, 180, -180, then use the rotate angle to determine wheel:
-            //                  0: if CW, quadrant 1 (front left); if CCW, quadrant 4 (front right)
-            //                  90: if CW, quadrant 2 (back left); if CCW, quadrant 1 (front left)
-            //                  180/-180: if CW, quadrant 3 (back right); if CCW, quadrant 2 (back left)
-            //                  -90: if CW, quadrant 4 (front right); if CCW, quadrant 3 (back right)
+          // Quad 1
+          if (angleChange > 0 && angleChange < 90) {
+            currentMode = spinoutMode.frontLeft;
           }
+
+          // Quad 2
+          if (angleChange > 90 && angleChange < 180) {
+            currentMode = spinoutMode.backLeft;
+          }
+
+          // Quad 3
+          if (angleChange > -180 && angleChange < -90) {
+            currentMode = spinoutMode.backRight;
+          }
+
+          // Quad 4
+          if (angleChange > -90 && angleChange < 0) {
+            currentMode = spinoutMode.frontRight;
+          }
+
+          // done
+          if (Math.abs(rotatePower) < Manual.spinoutRotateDeadBand) {
+            currentMode = spinoutMode.none;
+          }
+        }
+        // use state machine for rotating each wheel in each direction (8 cases)
+        // each module rotating CW and CCW
+        // if rotation stick falls under second deadband or robot rotates 90 degrees,
+        // reset rotation back to normal
+        switch (currentMode) {
+          case none:
+            drive.drive(driveX, driveY, rotatePower);
+            break;
+          case frontLeft:
+            drive.drive(driveX, driveY, rotatePower,
+                Constants.DriveConstants.frontLeftWheelLocation);
+            break;
+          case backLeft:
+            drive.drive(driveX, driveY, rotatePower,
+                Constants.DriveConstants.backLeftWheelLocation);
+            break;
+          case backRight:
+            drive.drive(driveX, driveY, rotatePower,
+                Constants.DriveConstants.backRightWheelLocation);
+            break;
+          case frontRight:
+            drive.drive(driveX, driveY, rotatePower,
+                Constants.DriveConstants.frontRightWheelLocation);
+            break;
+          case abort:
+            break;
+        }
     }
   }
 
