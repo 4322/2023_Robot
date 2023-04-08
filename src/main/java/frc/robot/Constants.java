a94e844db73a831cffbf181f3d39b88e77bcb944
zaydkhan@frc4322.com <zaydkhan@frc4322.com>
2023-04-08 11:22:27 -0700

    added values for proposed load position

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -296,7 +296,7 @@ public class Constants {
     public static final double maxPosition = 73;
     public static final double minPosition = 0;
 
-    public static final double loadPosition = 2;
+    public static final double loadPosition = 2; // TODO: proposed load position: 15
     public static final double loadHighPosition = 10;
     public static final double earlyTelescopeExtendPosition = 40;
     public static final double safeTelescopeExtendPosition = 57.0;
@@ -333,7 +333,7 @@ public class Constants {
     public static final double maxPosition = 13.0;
     public static final double minPosition = 0;
 
-    public static final double loadPosition = 0;
+    public static final double loadPosition = 0; // TODO: proposed load position: 1.5
     public static final double earlyArmRetractPosition = 9;
     public static final double safeArmRetractPosition = 0.5;  // safe for overhead/hopper clearance
     public static final double lowScoringPosition = 0;
