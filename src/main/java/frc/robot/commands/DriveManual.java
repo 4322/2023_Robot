9aa982aaf888c2e6b95b4ab93ad714c8c7814937
Amelia Schroeder <ameliaschroeder@frc4322.com>
2023-09-22 19:20:50 -0700

    removed dual error negation

--- src/main/java/frc/robot/commands/DriveManual.java
+++ src/main/java/frc/robot/commands/DriveManual.java
@@ -129,7 +129,7 @@ public class DriveManual extends CommandBase {
 
         if (Constants.driveTuningMode) {
           // quantize input drive magnitude to 0, 0.25, 0.5, 0.75, 1.0 for PID tuning
-          driveMag = Math.round(driveMag * 4.0 + 0.499) / 4.0;
+          driveMag = Math.round(driveMag * 4.0) / 4.0;
         }
 
         driveMag = driveMag * driveMag * driveMag; // Increase sensitivity efficiently
