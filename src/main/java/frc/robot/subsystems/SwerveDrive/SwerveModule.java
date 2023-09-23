1f9c0f000e348c25f90acde4d3e45a93da1562b4
Ryan Vu <ryanvu@frc4322.com>
2023-09-22 22:13:00 -0700

    Fix rotations per second equation

--- src/main/java/frc/robot/subsystems/SwerveDrive/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive/SwerveModule.java
@@ -151,7 +151,7 @@ public class SwerveModule extends ControlModule {
 
         driveMotor.setControl(new VelocityVoltage(state.speedMetersPerSecond
                 / (DriveConstants.Drive.wheelDiameterInches * Constants.inchesToMeters * Math.PI)
-                * DriveConstants.Drive.gearRatio * DriveConstants.encoderResolution / 10));
+                * DriveConstants.Drive.gearRatio));
         turningMotor.getPIDController().setReference(
           MathUtil.inputModulus(state.angle.getDegrees(), 0, 360), 
           ControlType.kPosition);
