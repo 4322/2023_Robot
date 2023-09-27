b2758bedbd615486c6ce2ac173733f183680aa09
Doug Kavner <dougkavner@frc4322.com>
2023-09-26 22:02:35 -0700

    Fix activation of next auto rotate

--- src/main/java/frc/robot/commands/ClawStop.java
+++ src/main/java/frc/robot/commands/ClawStop.java
@@ -3,20 +3,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Claw;
   
-  public class ClawStop extends InstantCommand {
+public class ClawStop extends InstantCommand {
 
-    public ClawStop() {
-      addRequirements(Claw.getInstance());
-    }
-  
-    @Override
-    public void initialize() {
-      Claw.getInstance().changeState(Claw.ClawMode.stopped);
-    }
-  
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-    }
+  public ClawStop() {
+    addRequirements(Claw.getInstance());
   }
-  
+
+  @Override
+  public void initialize() {
+    Claw.getInstance().changeState(Claw.ClawMode.stopped);
+    ArmMove.setArmPreset(ArmMove.Position.loadSingle);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+}
